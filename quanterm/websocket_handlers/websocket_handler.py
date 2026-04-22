5f2982d6b07945d567f509f946e222984fdc2680
cb3xd <cesar_basco@proton.me>
2026-04-22 17:59:00 +0800

    Removed universal out_queue from websocket handler

--- quanterm/websocket_handlers/websocket_handler.py
+++ quanterm/websocket_handlers/websocket_handler.py
@@ -1,22 +1,20 @@
 from quanterm.data.events import StreamType
-from pydantic import BaseModel
 import ccxt.pro as ccxtpro
 import asyncio
 
 
 class WebsocketHandler:
-    def __init__(self, exchange_id: str, out_queue: asyncio.Queue) -> None:
+    def __init__(self, exchange_id: str) -> None:
         self.exchange = getattr(ccxtpro, exchange_id)()
-        self.out_queue: asyncio.Queue[Any] = out_queue
         self._active_streams: dict[str, asyncio.Task] = {}
 
-    async def subscribe(self, pair: str, stream_type: StreamType):
+    async def subscribe(self, pair: str, stream_type: StreamType, out_queue: asyncio.Queue):
         stream_id = f"{pair}@{stream_type}"
         if stream_id in self._active_streams:
             print("Stream already exists!")
             return stream_id
         self._active_streams[stream_id] = asyncio.create_task(
-            self._watch_stream(stream_id=stream_id)
+            self._watch_stream(stream_id=stream_id, out_queue=out_queue)
         )
         return stream_id
 
@@ -33,14 +31,14 @@ class WebsocketHandler:
     async def close(self):
         await self.exchange.close()
 
-    async def _watch_stream(self, stream_id: str):
+    async def _watch_stream(self, stream_id: str, out_queue: asyncio.Queue):
         backoff = 1
         pair = stream_id.split("@")[0]
         stream_type = stream_id.split("@")[1]
         while self._active_streams.get(stream_id):
             try:
                 events = await getattr(self.exchange, stream_type)(pair)
-                await self.out_queue.put(events)
+                await out_queue.put(events)
                 backoff = 1
 
             except Exception:

ac129ac814d954d4136fa7616d2710bddd5406d7
cb3xd <cesar_basco@proton.me>
2026-05-02 20:42:55 +0800

    Start on frontend

---
+++ frontend/src/App.tsx
@@ -0,0 +1,11 @@
+import TabSwitcher from "./Tabs";
+
+function App() {
+  return (
+    <>
+      <TabSwitcher></TabSwitcher>
+    </>
+  );
+}
+
+export default App;
---
+++ frontend/src/Footer.tsx
@@ -0,0 +1,9 @@
+function Footer() {
+  return (
+    <footer>
+      <p>&copy; {new Date().getFullYear()} Quanterm</p>
+    </footer>
+  );
+}
+
+export default Footer;

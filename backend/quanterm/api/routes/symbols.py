736aa283d256b88842a73f73a45471a4e6e9d12a
cb3xd <cesar_basco@proton.me>
2026-05-25 20:28:29 +0800

    Set conversion to FastAPI compatible type using msgspec

--- backend/quanterm/api/routes/symbols.py
+++ backend/quanterm/api/routes/symbols.py
@@ -1,9 +1,13 @@
-from fastapi import APIRouter
+from fastapi import APIRouter, Response
+from msgspec import json
 from quanterm.exchange.exchange_manager import manager
 
 router = APIRouter()
+_encoder = json.Encoder()
 
 
 @router.get("/all_exchange_symbols")
 async def get_all_exchange_symbols():
-    return await manager.get_all_symbols()
+    symbols = await manager.get_all_symbols()
+    serialized_bytes = _encoder.encode(symbols)
+    return Response(content=serialized_bytes, media_type="application/json")

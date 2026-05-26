ea5605e3d6839637e0f9f079bfa31c9f21e9c0a2
cb3xd <cesar_basco@proton.me>
2026-05-26 16:51:43 +0800

    CORS fix

--- backend/quanterm/api/main.py
+++ backend/quanterm/api/main.py
@@ -4,6 +4,7 @@ from quanterm.api.routes.websocket import router as ws_router
 from quanterm.api.routes.symbols import router as symbols_router
 from quanterm.exchange.constants import ExchangeID
 from quanterm.exchange.exchange_manager import manager
+from fastapi.middleware.cors import CORSMiddleware
 
 
 @asynccontextmanager
@@ -14,5 +15,12 @@ async def lifespan(app: FastAPI):
 
 
 app = FastAPI(lifespan=lifespan)
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=["http://localhost:5173"],
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
 app.include_router(ws_router)
 app.include_router(symbols_router, prefix="/api")

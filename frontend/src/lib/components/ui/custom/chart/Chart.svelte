7207e20f51fa70320ad37499984cb5f3776a7aef
cb3xd <cesar_basco@proton.me>
2026-09-07 06:32:00 +0800

    Fixes: replaced old chart component with new

--- frontend/src/lib/components/ui/custom/chart/Chart.svelte
+++ frontend/src/lib/components/ui/custom/chart/Chart.svelte
@@ -1,32 +1,32 @@
 <script lang="ts">
-  import { onMount, onDestroy } from "svelte";
+  import { onDestroy, onMount } from "svelte";
+  import { Chart } from "./Chart.ts";
   import * as PIXI from "pixi.js";
-  import { Text } from "pixi.js";
-  import { chartsStore } from "./chartsDataStore.svelte";
-  let container: HTMLDivElement;
+  import { chartsStore } from "./chartsDataStore.svelte.js";
   let app: PIXI.Application;
+  let chart: Chart;
+  let container: HTMLDivElement;
+  let text: PIXI.Text;
 
-  let yAxisBar: PIXI.Graphics;
-  let txt: PIXI.Text;
   onMount(async () => {
     app = new PIXI.Application();
     await app.init({
       background: "#000000",
       resizeTo: container,
+      sharedTicker: true,
     });
     container.appendChild(app.canvas);
-    yAxisBar = new PIXI.Graphics()
-      .rect(0, 0, container.clientWidth / 24, container.clientHeight)
-      .fill("#101010");
-    yAxisBar.position.set(
-      container.clientWidth - container.clientWidth / 24,
-      0,
-    );
-    yAxisBar.eventMode = "static";
-    yAxisBar.cursor = "ns-resize";
-    console.log(container.clientWidth - container.clientWidth / 24);
 
-    txt = new Text({
+    chart = new Chart({
+      container: container,
+      events: app.renderer.events,
+      ticker: app.ticker,
+    });
+
+    app.stage.addChild(chart);
+    chart.drag().wheel();
+
+    text = new PIXI.Text({
       text: "Press '+' to add a chart",
       style: {
         fontFamily: "Arial",
@@ -35,35 +35,20 @@
       },
     });
 
-    app.ticker.add(updateText);
-    function updateText() {
-      txt.text =
+    text.anchor.set(0.5);
+    text.position.set(container.clientWidth / 2, container.clientHeight / 2);
+    app.ticker.add(() => {
+      text.text =
         chartsStore.currentChart !== undefined
           ? chartsStore.currentChart.ticker.toUpperCase()
           : "Press '+' to add a chart";
-    }
-    txt.anchor.set(0.5);
-    txt.position.set(container.clientWidth / 2, container.clientHeight / 2);
-    app.stage.addChild(txt);
-
-    app.stage.addChild(yAxisBar);
-  });
-  window.addEventListener("resize", handleResize);
-  function handleResize() {
-    console.log(container.clientHeight, container.clientWidth);
-    const newPos = container.clientWidth - container.clientWidth / 24;
-    console.log(newPos);
-    yAxisBar.position.set(newPos, 0);
-    yAxisBar.setSize(yAxisBar.width, container.clientHeight);
-  }
-
-  onDestroy(() => {
-    app?.destroy(true, { children: true });
+    });
+    app.stage.addChild(text);
   });
 </script>
 
 <div
   bind:this={container}
-  style="cursor: crosshair;"
+  style="cursor: crosshair"
   class="relative w-full flex-1 min-h-0 overflow-hidden"
 ></div>
